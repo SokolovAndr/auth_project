c9f8b5cefba0eec5326dbc2f6e92da898d30e363
andrei.sokolov <andrei.sokolov@stm-labs.ru>
2024-08-26 11:52:05 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'screens/account_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/reset_password_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/verify_email_screen.dart';
+import 'services/firebase_stream.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        }),
+      ),
+      routes: {
+        '/': (context) => const FirebaseStream(),
+        '/home': (context) => const HomeScreen(),
+        '/account': (context) => const AccountScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen(),
+        '/verify_email': (context) => const VerifyEmailScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'account_screen.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Главная страница',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            onPressed: () {
+              if ((user == null)) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              } else {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AccountScreen()),
+                );
+              }
+            },
+            icon: Icon(
+              Icons.person,
+              color: (user == null) ? Colors.black : Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: (user == null)
+              ? const Text("Контент для НЕ зарегистрированных в системе")
+              : const Text('Контент для ЗАРЕГИСТРИРОВАННЫХ в системе'),
+        ),
+      ),
+    );
+  }
+}
